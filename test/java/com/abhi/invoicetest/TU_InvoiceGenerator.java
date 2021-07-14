8165c8005d817129b63e865a37e52499fd3a814a
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2021-07-15 00:44:49 +0530

    Refactor: Checking return ride list of userId matches with expected list / not

--- test/java/com/abhi/invoicetest/TU_InvoiceGenerator.java
+++ test/java/com/abhi/invoicetest/TU_InvoiceGenerator.java
@@ -3,8 +3,10 @@ package com.abhi.invoicetest;
 import com.abhi.invoicegenerator.EnhancedInvoice;
 import com.abhi.invoicegenerator.InvoiceGenerator;
 import com.abhi.invoicegenerator.Ride;
+import com.abhi.invoicegenerator.RideRepository;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import java.util.List;
 /**
  * TU_InvoiceGenerator  --  Defining Test Cases for all operations performed
  *
@@ -35,7 +37,6 @@ public class TU_InvoiceGenerator {
         double fare = invoiceGenerator.calculateFare(distance,time);
         Assert.assertEquals(invoiceGenerator.MINIMUM_FARE, fare,0.0);
     }
-
     /**
      * This method passes array containing multiple ride data to get
      * the aggregate total fare of all rides & checks the fare return
@@ -64,5 +65,37 @@ public class TU_InvoiceGenerator {
         Assert.assertEquals(2,invoice.getTotalNoOfRides());
         Assert.assertEquals(65,invoice.getTotalFare(),0.0);
         Assert.assertEquals(32.5,invoice.getAvgFarePerRide(),0.0);
+    }
+
+    /**
+     * This method checks when userId is given the return ride list
+     * of given userId is matches with expected list / not
+     */
+    @Test
+    void whenUserIdIsGiven_ReturnListOfRidesOfUserId_ReturnTrueIfListIsCorrect() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        RideRepository rideRepository = new RideRepository();
+        //adding multiple userId with rides data
+        int userId1 = 101;
+        Ride[] ridesOfUserId_101 = { new Ride(5,10),
+                                    new Ride(4,8)
+                                    };
+        rideRepository.addUserToRideRepository(userId1, ridesOfUserId_101);
+        int userId2 = 102;
+        Ride[] ridesOfUserId_102 = { new Ride(7,20),
+                                    new Ride(3,8)
+                                    };
+        rideRepository.addUserToRideRepository(userId2, ridesOfUserId_102);
+        int userId3 = 103;
+        Ride[] ridesOfUserId_103 = { new Ride(8,30),
+                                    new Ride(2,8)
+                                    };
+        rideRepository.addUserToRideRepository(userId3, ridesOfUserId_103);
+        //getting ride list of userId
+        List<EnhancedInvoice> listOfUserRides = invoiceGenerator.getListOfRidesByUserId(userId2);
+        //checking with expected list
+        Assert.assertEquals(2,listOfUserRides.get(0).getTotalNoOfRides());
+        Assert.assertEquals(128.0,listOfUserRides.get(0).getTotalFare(),0.0);
+        Assert.assertEquals(64.0,listOfUserRides.get(0).getAvgFarePerRide(),0.0);
     }
 }
