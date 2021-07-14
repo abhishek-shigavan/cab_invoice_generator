e46e45b79390f5dd0b4324939c1dceb7c398d01c
abhishek-shigavan <abhishekshigavan1437@gmail.com>
2021-07-14 18:31:11 +0530

    Add: Computing Total Fare Of Multiple Rides

--- main/java/com/abhi/invoicegenerator/InvoiceGenerator.java
+++ main/java/com/abhi/invoicegenerator/InvoiceGenerator.java
@@ -1,7 +1,7 @@
 package com.abhi.invoicegenerator;
 
 /**
- * InvoiceGenerator --  Calculating Fare of Ride
+ * InvoiceGenerator --  Computing Fare of Cab Ride
  *
  * @author Abhishek Shigavan
  */
@@ -25,6 +25,28 @@ public class InvoiceGenerator {
         totalFare = distance * COST_PER_KILOMETER + time * COST_PER_MINUTE;
         if(totalFare < MINIMUM_FARE) {
             return MINIMUM_FARE;
+        }
+        return totalFare;
+    }
+    /**
+     * This method takes array containing multiple rides data as a
+     * input & fetches the values from array to compute fare
+     * It will compute fare & every time keep adding it to total fare to
+     * get aggregate fare of all rides & return the same
+     *
+     * @param rideArray - array containing multiple ride data
+     * @return totalFare - aggregate total fare of all rides
+     */
+    public double calculateFare(Ride[] rideArray) {
+        double totalFare = 0.0;
+        for (Ride ride : rideArray) {
+            double fare = ride.distance * COST_PER_KILOMETER + ride.time * COST_PER_MINUTE;
+            //assuring fare less than minimum fare
+            // will not be added to total fare
+            if(fare < MINIMUM_FARE){
+                fare = MINIMUM_FARE;
+            }
+            totalFare += fare;
         }
         return totalFare;
     }
